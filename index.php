1be80e928b4e836a04d5868dab640f25ac3c3786
Pichau <Pichau@DESKTOP-9T26SOC>
2021-01-15 19:06:22 -0200

    Projetokitcaker.

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+//Meu código vem aqui.
+
+$pg = isset($_GET['pg']);
+
+if ($pg) {
+    switch ($_GET['pg']) {
+
+        case 'inicio':
+            // Incluir a página inicio
+
+
+            include_once 'paginas/includes/header.php';
+            include_once 'paginas/inicio.php';
+            include_once 'paginas/includes/footer.php';
+            break;
+
+        case 'cadastro':
+            // Incluir a página contato
+
+           
+            include_once 'paginas/cadastro.php';
+            
+
+            break;
+
+
+
+        case 'produtos':
+            // Incluir a página produtos
+
+            
+            include_once 'paginas/produtos.php';
+            
+            break;
+        case 'obrigado':
+            // Incluir a página produtos
+
+            
+            include_once 'paginas/obrigado.php';
+            
+            break;
+        
+        
+
+
+        default:
+            // Página de Erro.
+            echo 'Página não encontrada';
+            break;
+    }
+} else {
+    include_once 'paginas/includes/header.php';
+    include_once 'paginas/inicio.php';
+    include_once 'paginas/includes/footer.php';
+}
+?>
---
+++ paginas/cadastro.php
@@ -0,0 +1,143 @@
+<link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css" />
+<link rel="stylesheet" href="plugins/icomoon/icomoon.css" />
+<link rel="stylesheet" href="assets/css/header.css" />
+<link rel="stylesheet" href="assets/css/kitcakes.css" />    
+<body>
+    <div class="manu">
+        <div class="container ">
+
+            <div class="container vini">
+                <nav class="navbar navbar-expand-lg navbar-dark vini">
+
+                    <a class="navbar-brand" href="?pg=inicio">
+                        <img src="assets/img/WhatsApp Image 2021-01-15 at 11.51.10.jpeg" width="100" height="100" class="d-inline-block align-top" alt="">
+
+                    </a>
+
+                    <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                        <ul class="navbar-nav mr-auto">
+
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0 ">
+                            <ul class="navbar-nav mr-auto"> 
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="?pg=inicio"><h5>Inicio</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="?pg=produtos"><h5>Produtos</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="?pg=cadastro"><h5>Cadastro</h5> <span class="sr-only">(página atual)</span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-facebook2"></span></h5></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-instagram"></h5></span></a>
+                                </li>                                                      
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#"> <h5><span class="reg-twitter"></h5></span></a>
+                                </li>                                                      
+                            </ul>
+                        </form>
+                    </div>
+            </div>
+            
+            <div class="container">
+
+                <div class="container mt-3">
+                    <h1 class="text-center pb-2 ">Cadastro</h1>
+                    <div class="form w-75 h-50 mx-auto ">
+                        <div class="row justify-content-center">
+                            <div class="col-6">
+
+                                <div class="form-group">
+                                    <span class="reg-user"></span>  
+                                    <label for="exampleFormControlInput1">Nome completo</label>                           
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>
+                                <div class="form-group">
+                                    <span class="reg-whatsapp"></span>  
+                                    <label for="exampleFormControlInput1">Celular</label>
+
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>
+                                <div class="form-group">
+                                    <span class="reg-mail"></span>
+                                    <label for="exampleFormControlInput1">E-mail</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>
+                                <div class="form-group">
+                                    <span class="reg-news-paper"></span>
+                                    <label for="exampleFormControlInput1">CPF</label>
+                                    <span class="manu-document"></span>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div> 
+                                <div class="form-group">
+                                    <span class="reg-barcode"></span>
+                                    <label for="exampleFormControlInput1">Código do produto</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="form-group">
+                                    <span class="reg-cakephp"></span>
+                                    <label for="exampleFormControlSelect1">Sabor da massa</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Chocolate</option>
+                                        <option>baunilha</option> 
+                                    </select>
+                                </div>       
+                                <div class="form-group">
+                                    <span class="reg-cakephp"></span>
+                                    <label for="exampleFormControlSelect1">Sabor do recheio</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Chocolate</option>
+                                        <option>Leite ninho</option> 
+                                        <option>Maracujá</option> 
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <span class="reg-cakephp"></span>
+                                    <label for="exampleFormControlSelect1">Nossas opções de kit</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>kit 1</option>
+                                        <option>Kit 2</option> 
+                                        <option>Kit 3</option> 
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <span class="reg-cakephp"></span>
+                                    <label for="exampleFormControlSelect1">Peso da torta</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>1 kg</option>
+                                        <option>2 kg</option>
+                                        <option>3 kg</option>
+
+                                    </select>
+                                </div> 
+                                <div class="form-group">
+                                    <span class="reg-credit-card1"></span>
+                                    <label for="exampleFormControlSelect1">Forma de pagamento</label>
+                                    <select class="form-control" id="exampleFormControlSelect1">
+                                        <option>Crédito</option>
+                                        <option>Débito</option>
+                                        <option>Boleto</option>
+                                        <option>Outros</option>
+                                    </select>
+                                </div> 
+                            </div>
+                        </div>
+                        <div class="form-group w-100 ">
+                            <span class="reg-chat-alt-stroke"></span>
+                            <label for="exampleFormControlTextarea1">Observação</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                        <a href="?pg=obrigado" class="btn btn-lg active btn-block cor" role="button" aria-pressed="true">Enviar Pedido</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
