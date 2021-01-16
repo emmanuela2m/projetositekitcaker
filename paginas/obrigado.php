a9363a9c55a3b84c0e48fbae98cfd1fb58ba3670
Pichau <Pichau@DESKTOP-9T26SOC>
2021-01-16 15:08:29 -0200

    alteraçoes da página obrigado, 16/01/2021

--- paginas/obrigado.php
+++ paginas/obrigado.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <link  rel="shortcut icon"  href="assets/img/favicon.ico">
-        <title>CodePen - A Pen by test</title>
+        <title>Obrigado</title>
         <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
         <link rel="stylesheet" href="plugins/pgobrigado/dist/style.css">
         <link rel="stylesheet" href="assets/css/header.css" />
@@ -12,12 +12,12 @@
     <body>
         <div class="error-rescue-modal servidor">
 
-            <div class="error-rescue-modal-mensagem "style="background-color: #8B4513;">
+            <div class="error-rescue-modal-mensagem mr-auto"style="background-color: #8B4513;">
                 <div class="error-rescue-modal-cabecalho" style="background-color: #8B4513;"><span class="error-rescue-modal-titulo">Obrigado!</span></div>
 
-                Seu pedido acaba de ser registrado! Agradecemos a sua preferência e confiança nos nossos produtos. Verifique sua caixa de entrada do Email ou spam pois logo entraremos em contato para enviar os valores à serem pagos. Deixaremos claro o dia e hora disponíveis para busca do kit/torta. Caso não encontre o Email, ligue para nosso número de contato (81) 98765-9878 ou fale conosco no Whatsapp nesse mesmo número.
+                <p class="text-justify" >Seu pedido acaba de ser registrado! Agradecemos a sua preferência e confiança nos nossos produtos. Verifique sua caixa de entrada do Email ou spam pois logo entraremos em contato para enviar os valores à serem pagos. Deixaremos claro o dia e hora disponíveis para busca do kit/torta. Caso não encontre o Email, ligue para nosso número de contato (81) 98765-9878 ou fale conosco no Whatsapp nesse mesmo número.
 
-                Obs: O pagamento só poderá ser efetuado no local de entrega.
+                    Obs: O pagamento só poderá ser efetuado no local de entrega.</p>
                 <div class="error-rescue-modal-rodape">
                     <a href="?pg=inicio" class="btn vini" style="margin-left: 93%">OK</a>
                 </div>
